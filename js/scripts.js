f65134f9e5852131dae0d7b935fc1f1f3b9bc0e1
Santiago Cianciulli <santiagoc81@outlook.com>
2024-04-14 23:28:38 -0700

    Me faltaba pasar 1 parametro

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //Inicializo las variables que voy a usar
+const servicios = ['Hosting', 'Web']
 // servicio:  via prompt para que el usuario elija entre Diseño web o hosting
 let servicio=''; // Scope Global, la ingresare via prompt
 // hostingSize: si elige hosting, le pedire el tamaño 
@@ -16,20 +17,30 @@ let valorTotal = 0;
 //Aviso que sucederá
 alert('Para solicitar tu presupuesto te solicitaremos: \n 1) El tipo de servicio. \n 2) Tamaño o cantidad de paginas');
 // Pido los datos
-pidoDatos()
+pidoDatos(servicios)
 // Calculo el presupuesto
 calculoPresupuesto()
 // Lo imprimo en la consola
 imprimoPresupuesto()
 
 // Funciones
-function pidoDatos() {
-    const servicios = ['Hosting', 'Web']
+function pidoDatos(servicios) {
+    
+    let totalServicios= servicios.length;
+    console.log(totalServicios);
+    textoPrompt = 'Ingrese el servicio a contratar (case sensitive): \n'
+    // Construyo el texto del prompt de forma dinamica
+    for (let i=0;i<totalServicios;i++){
+        textoPrompt= textoPrompt + "- " + servicios[i] + '\n'
+    }
+
     do {
-        servicio = prompt("Ingrese el servicio a contratar: \n - Hosting \n - Web")
+        servicio = prompt(textoPrompt)
         
     } while((servicio != servicios[0])&&(servicio != servicios[1]));
+    
     console.log('El servicio seleccionado es: ' + servicio)
+    
     switch (servicio) {
         case 'Hosting':
             hostingSize= parseInt(prompt("Ingrese la cantidad de sitios webs que desea almacenar "))
@@ -40,17 +51,18 @@ function pidoDatos() {
             requireAnimations= confirm("Necesita tener animaciones? \n Si -> OK \n No -> Cancel")
         break;
     }
+    
 }
 
 function calculoPresupuesto() {
 
     if (servicio == 'Hosting') {
-        valorTotal = (hostingSize * 50) + (emailCount *5)
+        valorTotal =(hostingSize,emailCount)=> (hostingSize * 50) + (emailCount *5)
         presupuesto = { 
             product: 'Hosting',
             sites: hostingSize,
             emails: emailCount,
-            total: "$" + valorTotal
+            total: "$" + valorTotal(hostingSize,emailCount)
         }
     }
     else
